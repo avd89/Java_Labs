96979d412c72fa50ecf9c66e0a6f72b9e73c08f3
Aliaksei Dubrouski <32129817+avd89@users.noreply.github.com>
2018-03-04 23:29:20 +0300

    Add files via upload

--- 1stLab/FirstLab.java
+++ 1stLab/FirstLab.java
@@ -1,13 +1,12 @@
-
-
-
 public class FirstLab {
     public static void main(String[] args){
-        System.out.printf("%-40s%-40s%n","Surname: ","Dubrovskii");
-        System.out.printf("%-40s%-40s%n","First Name: ","Aleksei");
-        System.out.printf("%-40s%-40s%n","Patronymic: ","Victorovich");
-      	System.out.printf("%-40s%-40s%n","Number of group: ","POIS1709v1");
-	System.out.printf("%-40s%-40s%n","Current occupation: ","Electronic engineer");
-	System.out.printf("%-40s%-40s%n","Hobbies: ","Planet Earth");
-	}
+          System.out.printf("%-40s%-40s\n","Surname: ","Dubrovskii");
+          System.out.printf("%-40s%-40s\n","First Name: ","Aleksei");
+          System.out.printf("%-40s%-40s\n","Patronymic: ","Victorovich");
+      	  System.out.printf("%-40s%-40s\n","Number of group: ","POIS1709v1");
+	  System.out.printf("%-40s%-40s\n","Current occupation: ","Electronic engineer");
+	  System.out.printf("%-40s%-40s\n","Hobbies: ","Planet Earth");
+	  }
 }
+
+
