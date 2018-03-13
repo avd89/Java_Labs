3ab34e91aa66ca36e1166143914fa31deee768b1
Aliaksei Dubrouski <32129817+avd89@users.noreply.github.com>
2018-03-13 16:09:18 +0300

    Add files via upload

---
+++ Lab5/JavaLab_05_06/JavaLab05_06/src/JavaLab05_06/JavaLab05_06.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaLab05_06;
+import java.util.Scanner;
+/**
+ * This is a program that show us the next day date
+ * Created by Aliaksei Dubrouski
+ * 13.03.2018
+ * POIS1709v1 group (BSU)
+ * @author Aliaksei Dubrouski
+ */
+public class JavaLab05_06 {
+    public static void main(String[] args){
+    
+    View.print("****This programm show you the next day date****");
+    
+    int day = UserInput.input("\nEnter day : ");
+    int month = UserInput.input("\nEnter month : ");
+    int year = UserInput.input("\nEnter year : ");
+    
+    String nextDayDate = NextDayDate.getNextDayDate(day, month, year);
+    
+    View.print("Next date is : " + nextDayDate);   
+        
+    }
+        
+    
+}
